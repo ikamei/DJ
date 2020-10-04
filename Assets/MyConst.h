09d8057e97ce2f7d453a0b1d95a5a2556b5a56e7
ikamei <ikamei@163.com>
2020-10-04 12:22:27 +0800

    支持唱片UI

---
+++ Assets/MyConst.h
@@ -0,0 +1,13 @@
+/* MyConst.h --- 
+ * Filename: MyConst.h
+ * Created: Sun Oct  4 12:02:59 2020 (+0800)
+ */
+
+/* Copyright Weiguo Mao All Rights Reserved.
+ */
+
+public class MyConsts
+{
+    int ACTION_STATE_NORMAL = 0; 
+    int ACTION_STATE_PUSH = 1;
+}
